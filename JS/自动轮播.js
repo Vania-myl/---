c34312df4d84ccf5cb05c054fd0beb7f61c24288
YL M <MYL18723548734@outlook.com>
2025-02-16 14:18:13 +0800

    JS

---
+++ JS/自动轮播.js
@@ -0,0 +1,24 @@
+const slides = document.querySelectorAll('.slide')
+let currentIndex = 0
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+        if (i === index) {
+            slide.classList.add('active')
+        } else {
+            slide.classList.remove('active')
+        }
+    })
+}
+
+function nextSlide() {
+    currentIndex = (currentIndex + 1) % slides.length;
+    showSlide(currentIndex);
+}
+
+setInterval(nextSlide, 3000);
+
+
+showSlide(currentIndex)
+
+
+
---
+++ JS/轮播背景.js
@@ -0,0 +1,5 @@
+const num = document.getElementsByClassName('images-background')
+const img = document.getElementsByClassName('slide')
+if (images.slide.opacity == 1) {
+    imagesBackground.backgroundImage = img.src
+}
